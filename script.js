4bdaa79885b5d702432ac07144bdb0ce0fd9426a
artalaa <artalaha1@gmail.com>
2024-06-10 22:21:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,41 @@ document.getElementById('switchToSignUp').addEventListener('click', function(eve
     event.preventDefault();
     $('#signup-tab').tab('show');
 });
+
+let currentIndex = 0;
+    const slides = document.querySelectorAll('.category-slide');
+    
+    function showSlide(index) {
+        (document.querySelectorAll('.category-slide')).forEach((slide, i) => {
+            slide.style.transform = `translateX(-${index * 100}%)`;
+        });
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % (document.querySelectorAll('.category-slide')).length;
+        showSlide(currentIndex);
+    }
+ setInterval(nextSlide, 3000);
+
+
+src;"https://code.jquery.com/jquery-3.6.0.min.js">
+src;"https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+    $(document).ready(function(){
+        $('.product-slider').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+    });
+
